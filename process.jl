d93861f8aa750164aed12fa8a9c05bc45e49834f
Fernando Guevara Vasquez <fguevara@math.utah.edu>
2024-07-02 14:05:39 -0600

    Operating plot

--- process.jl
+++ process.jl
@@ -1,6 +1,6 @@
 import Literate
 
-inputs = ["example1","example2","example3"]
+inputs = ["thermal_noise","example1","example2","example3","example4"]
 for i ∈ inputs
   Literate.notebook("$(i).jl",i)
 end
--- thermal_noise.jl
+++ thermal_noise.jl
@@ -3,7 +3,7 @@
 # is equivalent to a deterministic problem
 # ## Graph setup
 # Define graph and graph Laplacian
-using Plots, LinearAlgebra, Test, Random
+using Plots, LinearAlgebra, Test, Random, Statistics
 ⊗ = kron
 Nx = 5; Ny = 5; # number of nodes
 x = (0:(Nx-1))*ones(1,Ny)
@@ -18,16 +18,17 @@ for i=1:(Nx-1), j=1:(Ny-1)
 end
 ∇ = [ I(Ny) ⊗ D(Nx) # horizontal edges
       D(Ny) ⊗ I(Nx) # vertical edges
-     DE ]
+      DE ]
 
-𝐁 = findall( (x[:].==0) .| (x[:].==Nx-1) .| (y[:].==0) .| (y[:].==Ny-1) )
+𝐁 = findall( (x[:].==0) .| (x[:].==Nx-1) )
 𝐈 = setdiff(1:Nx*Ny,𝐁)
 n𝐈 =length(𝐈); n𝐁 = length(𝐁); 
 n𝐄, n𝐕 = size(∇)
+R𝐈 = I(n𝐕)[𝐈,:]  # restriction to interior nodes
 
-#f = (y[𝐁] .== Ny-1) - (y[𝐁] .== 0) # boundary condition
-f = x[𝐁] + y[𝐁]/2
-σ = ones(n𝐄)
+f = (x[𝐁] .== Nx-1) - (x[𝐁] .== 0)# boundary condition
+#f = x[𝐁] + y[𝐁]/2
+σ = ones(n𝐄);
 
 # ## Graph plotting
 scatter(x[𝐈],y[𝐈], color="blue",markersize=7,markeralpha=0.5);
@@ -43,11 +44,10 @@ end
 p=plot!(legend=:none, aspect_ratio=:equal, axis=false, grid=false,size=(300,300))
 
 # ## Generate random noise currents
-# Note: this code stores all realizations and is un-necessarily memory intensive
-#Random.seed!(17) # initialize seed
-#κ = 1.380649e−23 # J/K
-κ = π
-T0 = 1; δT = 1e6; # background temp and perturbation
+# The code below stores all realizations so it is unnecessarily memory intensive. Also notice that the factor $\kappa/\pi$ appears in both the variance of the thermal noise currents and the end result. So instead of using Boltzmann's constant $\kappa \approx 1.380649 \times 10^{−23}$ J/K we can use $\kappa = \pi$ and any relative errors we calculate will be accurate. We also chose a very small background temperature (1K) because it allowed for a more accuarate result with fewer realizations.
+Random.seed!(17) # initialize seed
+κ = π # the correct Boltzmann constant is κ = 1.380649e−23 # J/K
+T0 = 1; δT = 100; # background temp and perturbation
 Nrel = 10000; # number of realizations
 
 L = ∇'*diagm(σ)*∇ # Laplacian
@@ -80,10 +80,33 @@ end
 fge = var(me,dims=2)
 
 stoch = fge .- fg0
-deter = (δT*κ/π)*σ.*abs2.(∇*udet)
+deter = (δT*κ/π)*σ.*abs2.(∇*R𝐈'*R𝐈*udet)
 
-println(stoch)
-println(deter)
+rel_error = norm(stoch - deter)/norm(deter)
+println("relative error = ", 100*rel_error,"%")
 
 plot(stoch,label="stoch")
 plot!(deter,label="deter")
+
+# ## Plots of deterministic vs stochastic dissipated power
+function plot_edge_quantity(f;lw=6)
+  p = plot()
+  maxf = maximum(f);
+  minf = minimum(f);
+  for (i, r) in enumerate(eachrow(∇))
+    i1, i2 = findall(abs.(r) .> 0)
+    if (maxf-minf)/(maxf+minf) < 1e-6
+      c = "black"
+    else
+      c = get(cgrad(:thermal),(f[i]-minf)/(maxf-minf))
+    end
+    plot!([x[i1], x[i2]], [y[i1], y[i2]], linecolor=c, lw=lw)
+  end
+  p=plot!(legend=:none, aspect_ratio=:equal, axis=false, grid=false)
+  return p
+end
+plot(
+ plot_edge_quantity(ones(n𝐄),lw=3),
+ plot_edge_quantity(deter,lw=6),
+ plot_edge_quantity(stoch,lw=6),
+ layout=(1,3))
